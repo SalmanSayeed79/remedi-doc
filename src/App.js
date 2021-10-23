7e8155b59d81095d92d52dbc9167fcf2419d53a9
Salman Sayeed <salmansayeed5345@gmail.com>
2021-10-23 17:28:26 +0600

    created routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import {ThemeProvider} from '@mui/material'
+import {createTheme} from '@mui/material/styles'
+import Home from './Pages/Home';
+import Appbar from './Components/Appbar';
+import Account from './Pages/Account'
+import Patients from './Pages/Patients'
+import PatientDetail from './Pages/PatientDetail'
+const theme=createTheme({
+  palette:{
+    primary:{
+      main:"#ff4081",
+      light:"#ff80ab",
+      dark:"#f50057",
+      contrastText:"#f4f4f4"
+    },
+    lightWhite:{
+      main:"#f4f4f4"
+    }
+  },
+  typography: {
+    fontFamily: [
+      "Bree Serif",
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+})
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    
+    <BrowserRouter>
+      <Appbar/>
+      <Switch>
+        <Route component={Home} exact path="/"/>
+        <Route component={Account} exact path="/account"/>
+        <Route component={Patients} exact path="/patients"/>
+        <Route component={PatientDetail} exact path="/patients/:id"/>
+      </Switch>
+    </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
